487e4d5d1c52994671681680322fa5d3f6473cf2
abdelrahman-assoum <abdelrahman.assoum@gmail.com>
2023-05-26 11:32:41 +0300

    finish add task design

--- src/Components/SmallButton/SmallButton.js
+++ src/Components/SmallButton/SmallButton.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./small.module.css";
 
 function SmallButton({ title, onClick }) {
-  return <button className={styles.smallButton} onClick={onClick}>{title}</button>;
+  return (
+    <button className={styles.smallButton} onClick={onClick}>
+      {title}
+    </button>
+  );
 }
 
 export default SmallButton;
--- src/Pages/Tasks/Tasks.js
+++ src/Pages/Tasks/Tasks.js
@@ -3,15 +3,23 @@ import Button from '../../Components/Button/Button'
 import SmallButton from '../../Components/SmallButton/SmallButton'
 import AddNewTask from '../../Components/AddNewTask/AddNewTask'
 
-function Tasks() {
 
+function Tasks() {
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const handleDialogOpen = () => {
+     setDialogOpen(true);
+  }
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  }
   return (
     <>
-    <div>
-    <SmallButton title="+ Add new task" onClick={()=> {setDialogOpen(true)}} />
-    </div>
+      <div>
+        <SmallButton title="+ Add new task" onClick={handleDialogOpen} variant='filled' />
+        <AddNewTask open={dialogOpen} onClose={handleDialogClose} />
+      </div>
     </>
-    )
+  );
 }
 
 export default Tasks
