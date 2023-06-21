cea1417c0dcc62b222e6abaf51a2fe50a71d3000
abdelrahman-assoum <abdelrahman.assoum@gmail.com>
2023-06-21 17:57:10 +0300

    some responsive fixings

--- src/context/authProvider.js
+++ src/context/authProvider.js
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
   }, [token, userInfo]);
 
 
-  const handleLogin = (token, userInfo) => {
+  const handleLoginSuccess = (token, userInfo) => {
     setToken(token);
     setUserInfo(userInfo);
   };
@@ -39,7 +39,7 @@ export const AuthProvider = ({ children }) => {
         token,
         userInfo,
         loading,
-        handleLogin,
+        handleLoginSuccess,
         handleLogout,
         setToken,
         setUserInfo,
